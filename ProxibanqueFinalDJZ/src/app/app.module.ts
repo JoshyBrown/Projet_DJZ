4085c64efa3de476205cfbea3aa7369a9ff0afb3
JoshyBrown <suraj.joshua@gmail.com>
2018-01-27 15:06:29 +0100

    liste-clients + liste-client-row = code commenté (pb bouchon json)

--- ProxibanqueFinalDJZ/src/app/app.module.ts
+++ ProxibanqueFinalDJZ/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -9,8 +8,6 @@ import { AppComponent } from './app.component';
 import { UpdateclientComponent } from './updateclient/updateclient.component';
 import { ListeClientsComponent } from './liste-clients/liste-clients.component';
 import { ListeClientRowComponent } from './liste-client-row/liste-client-row.component';
-
-
 
 @NgModule({
   declarations: [
--- ProxibanqueFinalDJZ/src/app/service/service-liste-clients.service.ts
+++ ProxibanqueFinalDJZ/src/app/service/service-liste-clients.service.ts
@@ -1,8 +1,13 @@
 import {Client} from '../model/client';
-import {Injectable} from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
+// import {Observable} from 'rxjs/Observable';
+// import {Http, Response} from '@angular/http';
+
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/catch';
 
 @Injectable()
-export class ServiceListeClientsService {
+export class ServiceListeClientsService implements OnInit {
 
   clients: Client[];
 
@@ -13,10 +18,33 @@ export class ServiceListeClientsService {
       new Client('3', 'Papi', 'Albert'),
     ];
   }
+  /*
+  constructor(private http: Http) {
+    
+    this.clients = [
+      new Client('1', 'Abidal', 'Franck'),
+      new Client('2', 'Mamie', 'Gigi'),
+      new Client('3', 'Papi', 'Albert'),
+    ];
+    
+  }
+  */
 
-    getAllClients() {
+  getAllClients() {
     console.log('service-liste-clients fonctionne');
     return this.clients;
+  }
+  /*
+  getAllClients(): Observable<any> {
+    console.log('service-liste-clients fonctionne');
+    return this.http.get('localhost:3000/clients')
+    .map((res: Response) => res.json())
+    .catch((error: any) => Observable.throw('Houston, on a un pb!'));
+  }
+  */
+
+  ngOnInit() {
+    console.log('mon serviceListeClients marche');
   }
 
 }
