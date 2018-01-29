9d973dffc772cab88aa15778e51c6153d097f385
JoshyBrown <suraj.joshua@gmail.com>
2018-01-30 00:22:08 +0100

    liste conseillers : service + component.ts = terminé. finaliser template

--- ProxibanqueFinalDJZ/src/app/liste-conseillers/liste-conseillers.component.ts
+++ ProxibanqueFinalDJZ/src/app/liste-conseillers/liste-conseillers.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { Conseiller } from '../model/conseiller';
+import { GerantConseillerService } from '../service/gerant-conseiller.service';
+import { AlertService } from '../service/alert.service';
+import { error } from 'util';
 
 @Component({
   selector: 'app-liste-conseillers',
@@ -11,9 +16,38 @@ export class ListeConseillersComponent implements OnInit {
 
   conseillers: Array<Conseiller>;
 
-  constructor() { }
+  constructor(
+    private gerantConseillerService: GerantConseillerService,
+    private alerService: AlertService,
+    private router: Router) { }
+
+  getAllConseilers() {
+    this.gerantConseillerService.getConseillersByGerant(JSON.parse(localStorage.getItem('currentUser')).id)
+    .subscribe(data => this.conseillers = data, error => this.alerService.error(error));
+
+    return false;
+  }
+
+  goToDetails(conseiller) {
+    this.router.navigate(['/update-conseiller', conseiller.id])
+  
+    return false;
+  }
+
+  deleteConseiller(conseiller: Conseiller) {
+    console.log('Conseiller à supprimer : ' + JSON.stringify(conseiller));
+
+    if (confirm("Supprimer le conseiller " + conseiller.nom + " " + conseiller.prenom + " ?")) {
+      this.gerantConseillerService.deleteConseiller(conseiller.id)
+      .subscribe(() => { this.alerService.success('Suppression du conseiller réussi')}, 
+      error => this.alerService.error(error));
+    }
+    return false;
+  }
 
   ngOnInit() {
+    console.log('liste-conseiller component marche');
+    this.getAllConseilers();
   }
 
 }
