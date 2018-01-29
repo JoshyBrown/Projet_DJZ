210a6bdee96e144c4c832e637df72cd594f2b661
JoshyBrown <suraj.joshua@gmail.com>
2018-01-29 12:11:06 +0100

    ajout comptes dans liste client. pas encore responsive

---
+++ ProxibanqueFinalDJZ/src/app/model/compte-courant.ts
@@ -0,0 +1,8 @@
+export class CompteCourant {
+    id: string;
+    numcompte: string;
+    solde: number;
+    etatActif: boolean;
+    dateOuverture: string;
+    decouvertAuthorise: number;
+}
---
+++ ProxibanqueFinalDJZ/src/app/model/compte-epargne.ts
@@ -0,0 +1,8 @@
+export class CompteEpargne {
+    id: string;
+    numcompte: string;
+    solde: number;
+    etatActif: boolean;
+    dateOuverture: string;
+    tauxInteret: number;
+}
