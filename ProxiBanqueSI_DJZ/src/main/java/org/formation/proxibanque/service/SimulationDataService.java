49b49c29846f13c4566af4640114cbdc6ea6b1f4
JoshyBrown <suraj.joshua@gmail.com>
2018-01-30 16:51:55 +0100

    vérification des champs

--- ProxiBanqueSI_DJZ/src/main/java/org/formation/proxibanque/service/SimulationDataService.java
+++ ProxiBanqueSI_DJZ/src/main/java/org/formation/proxibanque/service/SimulationDataService.java
@@ -46,34 +46,72 @@ public class SimulationDataService {
 		Adresse add = new Adresse("Paris", 75001, "IdF", "0123456789");
 
 		Client client01 = new Client("Dupont", "Julien", "CL001",
-				new Adresse("Palaiseau", 91120, "IdF", "01234567"));
-		Client client02 = new Client("CLIENT02", "client02", "CL002",
-				new Adresse("Palaiseau", 91120, "IdF", "01234567"));
-		Client client03 = new Client("CLIENT03", "client03", "CL003",
-				new Adresse("Palaiseau", 91120, "IdF", "01234567"));
-		Client client04 = new Client("CLIENT04", "client04", "CL004",
-				new Adresse("Palaiseau", 91120, "IdF", "01234567"));
-		Client client05 = new Client("CLIENT05", "client05", "CL005",
-				new Adresse("Palaiseau", 91120, "IdF", "01234567"));
+				new Adresse("Palaiseau", 91120, "IdF", "0123456700"));
+		client01.getCompteCourant().setEtatActif(true);
+		client01.getCompteCourant().setNumCompte("00001");
+		client01.getCompteCourant().setSolde(1200.44);
+		client01.getCompteCourant().setDecouvertAuthorise(1000.00);
+		
+		client01.getCompteEpargne().setEtatActif(true);
+		client01.getCompteEpargne().setNumCompte("10001");
+		client01.getCompteEpargne().setSolde(220000.00);
+		client01.getCompteEpargne().setTauxInteret(0.03);
+		
+		Client client02 = new Client("Leniaud", "Michelle", "CL002",
+				new Adresse("Palaiseau", 91120, "IdF", "0123450067"));
+		client02.getCompteCourant().setEtatActif(true);
+		client02.getCompteCourant().setNumCompte("00002");
+		client02.getCompteCourant().setSolde(200.05);
+		client02.getCompteCourant().setDecouvertAuthorise(1000.00);
+		
+		Client client03 = new Client("Durant", "Albert", "CL003",
+				new Adresse("Palaiseau", 91120, "IdF", "0120034567"));
+		client03.getCompteEpargne().setEtatActif(true);
+		client03.getCompteEpargne().setNumCompte("10003");
+		client03.getCompteEpargne().setSolde(500000.00);
+		client03.getCompteEpargne().setTauxInteret(0.03);
+		
+		Client client04 = new Client("Lewis", "Karl", "CL004",
+				new Adresse("Palaiseau", 91120, "IdF", "0123004567"));
+		client04.getCompteCourant().setEtatActif(true);
+		client04.getCompteCourant().setNumCompte("00004");
+		client04.getCompteCourant().setSolde(-420.44);
+		client04.getCompteCourant().setDecouvertAuthorise(1000.00);
+		
+		client04.getCompteEpargne().setEtatActif(true);
+		client04.getCompteEpargne().setNumCompte("10004");
+		client04.getCompteEpargne().setSolde(3001.00);
+		client04.getCompteEpargne().setTauxInteret(0.03);
+		
+		Client client05 = new Client("Duval", "Jean", "CL005",
+				new Adresse("Palaiseau", 91120, "IdF", "0123456007"));
 
 		Client enterprise1 = new Client("Darty", "EJ001883", "CL006", add);
 		Client enterprise2 = new Client("Carrefour", "EJ001223", "CL007", add);
 		Client enterprise3 = new Client("Auchan", "E001234", "CL008", add);
 		Client enterprise4 = new Client("SFR", "EJ9179875", "CL009", add);
 
-		Conseiller leConseiller = new Conseiller("David", "SUPER", "C001", add);
-		leConseiller.setLogin("conseiller");
-		leConseiller.setPassword(passwordEncoder.encode("test"));
+		Conseiller leConseiller1 = new Conseiller("David", "JOSHUA", "C001", add);
+		leConseiller1.setLogin("david");
+		leConseiller1.setPassword(passwordEncoder.encode("test"));
 		
-		leConseiller.addClient(client01);
-		leConseiller.addClient(client02);
-		leConseiller.addClient(client03);
-		leConseiller.addClient(client04);
-		leConseiller.addClient(client05);
-		leConseiller.addClient(enterprise1);
-		leConseiller.addClient(enterprise2);
-		leConseiller.addClient(enterprise3);
-		leConseiller.addClient(enterprise4);
+		Conseiller leConseiller2 = new Conseiller("Marc", "MARIN", "C002", add);
+		leConseiller2.setLogin("marc");
+		leConseiller2.setPassword(passwordEncoder.encode("test"));
+		
+		Conseiller leConseiller3 = new Conseiller("Zlatka", "GRAS", "C003", add);
+		leConseiller3.setLogin("zlatka");
+		leConseiller3.setPassword(passwordEncoder.encode("test"));
+		
+		leConseiller1.addClient(client01);
+		leConseiller1.addClient(client02);
+		leConseiller1.addClient(client03);
+		leConseiller1.addClient(client04);
+		leConseiller1.addClient(client05);
+		leConseiller1.addClient(enterprise1);
+		leConseiller1.addClient(enterprise2);
+		leConseiller1.addClient(enterprise3);
+		leConseiller1.addClient(enterprise4);
 
 		Gerant leGerant = new Gerant("WANG", "Julien", "CG002", add);
 		leGerant.setLogin("gerant");
@@ -82,7 +120,7 @@ public class SimulationDataService {
 		
 		Agence leAgence = new Agence("0001", LocalDateTime.now().toString(), leGerant);
 
-		leGerant.addConseiller(leConseiller);
+		leGerant.addConseiller(leConseiller1);
 		leAgence.setGerant(leGerant);
 
 		daoAgence.save(leAgence);
--- ProxibanqueFinalDJZ/src/app/app.module.ts
+++ ProxibanqueFinalDJZ/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 import { RouterModule } from '@angular/router';
 import { routing } from './app.routing';
