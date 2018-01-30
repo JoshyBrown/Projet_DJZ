444e59a28564f303d97b3759a589eb7ed732b62c
wangjiliang <wangjiliang@gmail.com>
2018-01-30 16:04:45 +0100

    Virement in progress

--- ProxibanqueFinalDJZ/src/app/virement/virement.component.ts
+++ ProxibanqueFinalDJZ/src/app/virement/virement.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpErrorResponse} from '@angular/common/http';
 
-import { Client } from '../model/client';
-import { Virement } from '../model/virement';
-import { ConseillerClientService } from '../service/conseiller-client.service';
-import { VirementService } from '../service/virement.service';
-import { AlertService } from '../service/alert.service';
+import {Client} from '../model/client';
+import {Compte} from '../model/compte';
+import {Virement} from '../model/virement';
+import {ConseillerClientService} from '../service/conseiller-client.service';
+import {VirementService} from '../service/virement.service';
+import {AlertService} from '../service/alert.service';
 
 @Component({
   selector: 'app-virement',
@@ -17,37 +18,76 @@ export class VirementComponent implements OnInit {
 
   clients: Client[];
   allClients: Client[];
-  
+
+  departs = new Array<Compte>();
+  cibles = new Array<Compte>();
+
   clientDebiteur: Client;
+  clientCrediteur: Client;
   
-  virement: Virement;
+  compteDepart: Compte;
+  compteCible: Compte;
+  
+  virement: Virement = new Virement();
 
   constructor(
     private conseillerCLientService: ConseillerClientService,
     private virementService: VirementService,
-    private alertService: AlertService) { }
-  
+    private alertService: AlertService) {}
+
   selectDebiteur(client: Client): void {
     console.log(JSON.stringify(client));
-  }
-  
-  selectCrediteur(client: Client): void {
-    console.log(JSON.stringify(client));
+    this.virement.debiteur = client;
+
+    if (client.compteCourant.etatActif) {
+      this.departs.push(client.compteCourant);
+    }
+    
+    if (client.compteEpargne.etatActif) {
+      this.departs.push(client.compteEpargne);
+    }
   }
 
+  selectCrediteur(client: Client): void {
+    console.log(JSON.stringify(client));
+    
+    this.virement.crediteur = client;
+    
+    if (client.compteCourant.etatActif) {
+      this.cibles.push(client.compteCourant);
+    }
+    
+    if (client.compteEpargne.etatActif) {
+      this.cibles.push(client.compteEpargne);
+    }
+  }
+  
+  selectDepart(compte: Compte) {
+    console.log(JSON.stringify(compte));
+    
+    this.virement.depart = compte;
+  }
+  
+  selectCible(compte: Compte) {
+    console.log(JSON.stringify(compte));
+    
+    this.virement.cible = compte;
+  }
+  
   onSubmit() {
-    console.log(JSON.stringify(this.clientDebiteur));
+
+    console.log(JSON.stringify(this.virement));
     this.virementService.doVirement(this.virement)
       .subscribe(data => this.virement = data, error => this.alertService.error(error.message));
   }
-  
+
   getClientsByConseiller() {
     this.conseillerCLientService.getClientsByConseiller(JSON.parse(localStorage.getItem('currentUser')).id)
       .subscribe(data => this.clients = data, error => this.alertService.error(error.message));
 
     return false;
   }
-  
+
   getAllClients() {
     this.conseillerCLientService.getClients()
       .subscribe(data => this.allClients = data, error => this.alertService.error(error.message));
